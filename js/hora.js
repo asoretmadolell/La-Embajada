69626f99aa8b1793ee818ed2980aa164e02afce7
Alejandro Soret <soret_40@hotmail.com>
2015-02-02 19:09:25 +0100

    La idea es que la temática de la web cambie dependiendo de si es de día o de noche en la vida real. Para ello, hemos llegado a una simple solución. Hemos creado un script que sacará la hora del cliente a través de un objeto "Date()", y si está entre las horas en las que se ocultará el sol (en este caso, las 19 y las 8), reemplazará todas las clases "dia" del DOM por la clase "noche". Por tanto, a partir de ahora, cada elemento que queramos que cambie con la hora sólo tiene que llevar la clase "dia" en el DOM, puesto que nuestro script se ocupará del resto. En el CSS, dichos elementos no ha habido que declararlos con la clase "dia", puesto que su imagen por defecto será la que corresponda al día. En cambio, sí ha habido que declararlos con la clase "noche" para que, en el caso de que sea de noche, sobrecargue la propiedad "background-image", consiguiendo el efecto que queremos. Por ahora, dejaremos la hora visible en el DOM, por si acaso. Además, en el script hemos dejado comentado un reloj por si nos hace falta en un futuro (obtiene la hora y refresca cada segundo, sencillo). También hemos cambiado la jerarquía de carpetas que usaremos para nuestras imágenes, evitando tener que renombrarlas.

---
+++ js/hora.js
@@ -0,0 +1,21 @@
+$(document).ready( function () {
+    obtenerHoraActual();
+} );
+
+function obtenerHoraActual() {
+    var fecha = new Date();
+
+    /* un relojito sencillo
+    var refresh = 1000; // 1000 mili segundos, o lo que es lo mismo, 1 segundo
+    window.setTimeout('obtenerHoraActual()', refresh);
+    var hora = fecha.toLocaleTimeString();
+    */
+
+    var hora = fecha.getHours();
+
+    $( '#hora' ).text( hora );
+
+    if( hora >= 19 || hora <= 8 ) {
+        $( '.dia' ).addClass( 'noche' ).removeClass( 'dia' );
+    }
+}
